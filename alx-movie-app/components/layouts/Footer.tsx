a07c5992d5bc1d1f9c6e309b1168b9adaec84514
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-12-05 14:38:06 +0300

    correction3

--- alx-movie-app/components/layouts/Footer.tsx
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-900 text-white p-4 text-center">
-      <p>&copy; 2025 CineSeek. All rights reserved.</p>
+      <p>&copy; 2025 CineSeek Movie. All rights reserved.</p>
     </footer>
   );
 };
